efd9d2ebb33d3ab93f3e23b5be5441782c525b92
nhg <nhg@igamingbet.org>
2019-02-11 14:32:49 +0800

    Site updated: 2019-02-11 14:32:48

--- js/insight.js
+++ js/insight.js
@@ -1,6 +1,6 @@
 /**
  * Insight search plugin
- * @author PPOffice { @link https://github.com/ppoffice }
+ * @author PPOffice { @link https://github.com/niuhonggui }
  */
 (function ($, CONFIG) {
     var $main = $('.ins-search');
